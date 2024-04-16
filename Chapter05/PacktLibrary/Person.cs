4c0bf11b7c3a246a28dd4592ff81747e48d44acb
Bruno de Souza Reis Costa <bruno.costa@evertical.com.br>
2024-04-16 19:39:29 -0300

    atualizando projeto com algumas propriedaes static, readonly e const

--- Chapter05/PacktLibrary/Person.cs
+++ Chapter05/PacktLibrary/Person.cs
@@ -3,4 +3,12 @@ public class Person
 {
     public string? Name { get; set; }
     public DateTime DateOfBirth;
+    public WondersOfTheAncientWorld FavoriteAncientWonder;
+    public WondersOfTheAncientWorld BucketList;
+    public List<Person> Children = new();
+    public const string Species = "Homo Sapiens";
+
+    //read-only
+    public readonly string HomePlanet = "Earth";
+    
 }
--- Chapter05/PeopleApp/Program.cs
+++ Chapter05/PeopleApp/Program.cs
@@ -1,6 +1,6 @@
 ﻿using Packt.Shared;
- 
- Person bob = new();
+
+Person bob = new();
  bob.Name = "Bruno";
  bob.DateOfBirth = new DateTime(1965, 12, 22);
 
@@ -8,3 +8,54 @@
     "{0} was born on {1:dddd, d MMMM yyyy}",
      arg0: bob.Name, 
      arg1: bob.DateOfBirth);
+
+Person alice = new(){
+   Name = "Alice Jone",
+   DateOfBirth = new(1998, 3, 7) //C# 9.0 or later
+};
+
+WriteLine(format: "{0} was born on {1:dd MMM yy}", 
+   arg0:alice.Name,
+   arg1:alice.DateOfBirth);
+
+bob.FavoriteAncientWonder = WondersOfTheAncientWorld.StatueOfZeusAtOlympia;
+WriteLine(
+   format:"{0}'s favorite wonder is {1}. Its integer is {2}.",
+   arg0: bob.Name,
+   arg1: bob.FavoriteAncientWonder,
+   arg2: (int)bob.FavoriteAncientWonder);
+
+bob.BucketList = WondersOfTheAncientWorld.HangingGardensOfBabylon | 
+   WondersOfTheAncientWorld.MausoleumAtHalicarnassus;
+// bob.BucketList = (WondersOfTheAncientWorld)18;
+
+WriteLine($"{bob.Name}'s bucket list is {bob.BucketList}");
+
+bob.Children.Add(new Person{Name = "Alfred"}); //C# 3.0 and later
+bob.Children.Add(new() {Name = "Zoe"}); //C# 9.0 and later
+WriteLine($"{bob.Name} has {bob.Children.Count} children:" );
+for (int childIndex = 0; childIndex < bob.Children.Count; childIndex++)
+{
+   WriteLine($"> {bob.Children[childIndex].Name}");
+}
+
+BankAccount.InterestRate = 0.012M; //store shared value
+BankAccount jonesAccount = new();
+jonesAccount.AccountName = "Mrs. Jones";
+jonesAccount.Balance = 2400;
+
+WriteLine(format: "{0} earned {1:C} interest.",
+   arg0: jonesAccount.AccountName,
+   arg1: jonesAccount.Balance * BankAccount.InterestRate);
+
+Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo.GetCultureInfo("en-GB");
+BankAccount gerrierAccount = new();
+gerrierAccount.AccountName = "Ms. Gerrier";
+gerrierAccount.Balance = 98;
+WriteLine(format: "{0} earned {1:C} ineterest.",
+   arg0: gerrierAccount.AccountName,
+   arg1: gerrierAccount.Balance * BankAccount.InterestRate);
+
+WriteLine($"{bob.Name} is a {Person.Species}");
+
+WriteLine($"{bob.Name} was born on {bob.HomePlanet}");
