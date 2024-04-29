801322ded0dd105f5e78cdd47ce72a5ac6e2698d
Bruno Souza <brunosrc@hotmail.com>
2024-04-29 17:59:23 -0300

    Working with records

--- Chapter05/PeopleApp/Program.cs
+++ Chapter05/PeopleApp/Program.cs
@@ -198,3 +198,40 @@ for (int i = 0; i < lamech.Children.Count; i++)
 }
 
 WriteLine($"5! is {Person.Factorial(5)}");
+
+
+Passenger[] passengers = {
+   new FirstClassPassenger {AirMiles = 1_419, Name = "Suman"},
+   new FirstClassPassenger {AirMiles = 16_562, Name = "Lucy"},
+   new BusinessClassPassenger {Name = "Janice"},
+   new CoachClassPassenger {CarryOnKG = 25.7, Name = "Dave"},
+   new CoachClassPassenger {CarryOnKG = 0, Name = "Amit"},
+};
+
+foreach (Passenger passenger in passengers)
+{
+   decimal flightCost = passenger switch
+   {
+      //C# 8 syntax
+      // FirstClassPassenger p when p.AirMiles > 35000 => 1500M,
+      // FirstClassPassenger p when p.AirMiles > 15000 => 1750M,
+      // FirstClassPassenger _ => 2000M,
+
+      //C# 9 or later syntax
+      // FirstClassPassenger p => p.AirMiles switch{
+      //    > 35000 => 1500M,
+      //    > 15000 => 1750M,
+      //    _       => 2000M
+      // },
+
+      FirstClassPassenger {AirMiles: > 35000} => 1500,
+      FirstClassPassenger {AirMiles: > 15000} => 1750M,
+      FirstClassPassenger                     => 1750M,
+
+      BusinessClassPassenger _ => 1000M,
+      CoachClassPassenger p when p.CarryOnKG < 10.0 => 500M,
+      CoachClassPassenger _ => 650M,
+      _ => 800M
+   };
+   WriteLine($"Flight costs {flightCost:C} for {passenger}");
+}
