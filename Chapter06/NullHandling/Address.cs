1078db977c322ed097e939106197e21f6680a1fa
Bruno de Souza <brunosrc@hotmail.com>
2024-05-30 01:34:20 -0300

    Declaring non-nullable variables and parameters

---
+++ Chapter06/NullHandling/Address.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NullHandling
+{
+    public class Address
+    {
+        public string? Building;
+        public string Street = string.Empty;
+        public string City = string.Empty;
+        public string Region = string.Empty;
+    }
+}
---
+++ Chapter06/NullHandling/Program.cs
@@ -0,0 +1,30 @@
+﻿//exemplo de como habilitar o desabilitar nullable no arquivo
+// #nullable disable
+
+using NullHandling;
+
+int thisCannotBeNull = 4;
+// thisCannotBeNull = null; //compile error!
+WriteLine(thisCannotBeNull);
+int? thisCouldBeNull = null;
+WriteLine(thisCouldBeNull);
+WriteLine(thisCouldBeNull.GetValueOrDefault());
+thisCouldBeNull = 7;
+WriteLine(thisCouldBeNull);
+WriteLine(thisCouldBeNull.GetValueOrDefault());
+
+//the actual type of int? is Nullable<int>
+Nullable<int> thisCouldBeAlsoNull = null;
+thisCouldBeAlsoNull = 9;
+WriteLine(thisCouldBeAlsoNull);
+
+Address address = new()
+{
+    Building = null,
+    Street = null!, //null forgiving operator
+    City = "London",
+    Region = "UK"
+};
+
+WriteLine(address.Building?.Length);
+WriteLine(address.Street.Length);
