f357b5ebc8e60581e533719e919fd51d1f9334f7
Bruno Souza <brunosrc@hotmail.com>
2024-06-05 20:30:59 -0300

    Fixing the code - Stylecop

--- Chapter06/CodeAnalyzing/Program.cs
+++ Chapter06/CodeAnalyzing/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// <copyright file="Program.cs" company="Packt">
+// Copyright (c) Packt. All rights reserved.
+// </copyright>
+
+namespace CodeAnalyzing;
+
+using System.Diagnostics;
+
+/// <summary>
+/// The main class for this console app.
+/// </summary>
+public class Program
+{
+    /// <summary>
+    /// The main entry point for this console app.
+    /// </summary>
+    /// <param name="args">
+    /// A string array of arguments passed to the console app.
+    /// </param>
+    public static void Main(string[] args)
+    {
+        Debug.WriteLine("Hello, Debugger!");
+    }
+}
