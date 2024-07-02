0269cbbc0425f1dd43e8c0b9a9d260365a4e5dfc
Bruno de Souza <brunosrc@hotmail.com>
2024-07-02 01:22:41 -0300

    Getting part of a string

---
+++ Chapter08/WorkingWithText/Program.cs
@@ -0,0 +1,20 @@
+﻿string city = "London";
+WriteLine($"{city} is {city.Length} characters long.");
+
+WriteLine($"First char is {city[0]} and fourth is {city[3]}.");
+
+string cities = "Paris,Tehran,Chennai,Sydney,New York,Medellín";
+string[] citiesArray = cities.Split(',');
+WriteLine($"There are {citiesArray.Length} items in the array: ");
+
+foreach (string item in citiesArray)
+{
+    WriteLine(item);
+}
+
+string fullName = "Alan Shore";
+int indexOfTheSpace = fullName.IndexOf(' ');
+string firstname = fullName.Substring(startIndex: 0, length: indexOfTheSpace);
+string lastName = fullName.Substring(startIndex: indexOfTheSpace + 1);
+WriteLine($"Original: {fullName}");
+WriteLine($"Swapped: {lastName}, {firstname}");
