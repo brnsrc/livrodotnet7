ddcca3a73f0dd4d05d814c3d47f86f2a433d718c
Bruno de Souza <brunosrc@hotmail.com>
2024-07-15 00:20:08 -0300

    Writing to XML streams

---
+++ Chapter09/WorkingWithStreams/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.Xml;
+using static System.IO.Path;
+using static System.Environment;
+using WorkingWithStreams;
+
+SectionTitle("Writing to text streams");
+//define a file to write to
+string textFile = Combine(CurrentDirectory, "streams.txt");
+//create a text file and return a helper writer
+StreamWriter text = File.CreateText(textFile); //ponta aonde vai escrever
+//enumerate the strings, writing each one
+//to the stream on a separate line
+foreach (string item in Viper.Callsigns)
+{
+    text.WriteLine(item); //conteúdo escrito
+}
+text.Close(); //release resources
+//output the contents of the file
+WriteLine("{0} contains {1:N0} bytes .",
+    arg0: textFile, arg1: new FileInfo(textFile).Length);
+WriteLine(File.ReadAllText(textFile));
+
+SectionTitle("Writing to XML streams");
+//define a file path to write to
+string xmlFile = Combine(CurrentDirectory, "streams.xml");
+//declare variables for the filestream and XML writer
+FileStream? xmlFileStream = null;
+XmlWriter? xml = null;
+
+try
+{
+    //create a file stream 
+    xmlFileStream = File.Create(xmlFile);
+    //wrap the file  stream in an XML writer  helper
+    //and automatically  indent nested elements
+    xml = XmlWriter.Create(xmlFileStream,
+        new XmlWriterSettings { Indent = true });
+
+    //write the XML declaration
+    xml.WriteStartDocument();
+    //write root element
+    xml.WriteStartElement("callSigns");
+    foreach (string item in Viper.Callsigns)
+    {
+        xml.WriteElementString("callsign", item);
+    }
+    //write the close root element
+    xml.WriteEndElement();
+    //close helper and stream
+    xml.Close();
+    xmlFileStream.Close();
+}
+catch (Exception ex)
+{
+    // if the path doesn't exist the exception will be caught
+    WriteLine($"{ex.GetType()} says {ex.Message}");
+}
+finally
+{
+    if (xml != null)
+    {
+        xml.Dispose();
+        WriteLine("The XML writer's unmanaged resources have been disposed.");
+        if (xmlFileStream != null)
+        {
+            xmlFileStream.Dispose();
+            WriteLine("The file stream's numanaged resources have been disposed.");
+        }
+    }
+}
+
+//output all the contents of the file 
+WriteLine("{0} contains {1:N0} bytes.", arg0: xmlFile,
+    arg1: new FileInfo(xmlFile).Length);
+WriteLine(File.ReadAllText(xmlFile));
---
+++ Chapter09/WorkingWithStreams/Viper.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkingWithStreams
+{
+    static class Viper
+    {
+        //define an array of Viper pilot call signs
+        public static string[] Callsigns = new[]{
+            "Husker", "Starbuck", "Apollo", "Boomer", "Bulldog",
+            "Athena", "Hello", "Racetrack"
+        };
+    }
+}
