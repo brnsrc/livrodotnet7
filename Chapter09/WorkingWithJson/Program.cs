2f36dfbacce14ad7061128caabd5b9b732545b48
Bruno de Souza <brunosrc@hotmail.com>
2024-07-26 01:54:07 -0300

    Controlling JSON processing

--- Chapter09/WorkingWithJson/Program.cs
+++ Chapter09/WorkingWithJson/Program.cs
@@ -4,7 +4,8 @@ using static System.IO.Path;
 using static System.Environment;
 
 
-Book mybook = new(title: "C# 11 and .NET 7 - Modern Cross-Platform Development Fundamentals"){
+Book mybook = new(title: "C# 11 and .NET 7 - Modern Cross-Platform Development Fundamentals")
+{
     Author = "Mark J Price",
     PublishDate = new(year: 2022, month: 11, day: 8),
     Pages = 823,
@@ -12,11 +13,24 @@ Book mybook = new(title: "C# 11 and .NET 7 - Modern Cross-Platform Development F
 };
 
 System.Console.WriteLine($"Mybook: {mybook.Title}, {mybook.Pages} pages.");
-JsonSerializerOptions option = new(){
+JsonSerializerOptions options = new()
+{
     IncludeFields = true, //include all fields
     PropertyNameCaseInsensitive = true,
     WriteIndented = true,
     PropertyNamingPolicy = JsonNamingPolicy.CamelCase
 };
 
-string filePath = Combine(CurrentDirectory, "mybook.json" );
+string filePath = Combine(CurrentDirectory, "mybook.json");
+using (Stream fileStream = File.Create(filePath))
+{
+    JsonSerializer.Serialize<Book>(
+        utf8Json: fileStream, value: mybook, options);
+}
+WriteLine("Written {0:N0} bytes of JSON to {1}",
+    arg0: new FileInfo(filePath).Length, arg1: filePath);
+WriteLine();
+//display the serialized object graph
+WriteLine(File.ReadAllText(filePath));
+
+
