582442680a32a88d545d2f151f017e042a97683c
Bruno de Souza <brunosrc@hotmail.com>
2024-08-16 00:43:53 -0300

    Setting up the dotnet-ef tool

--- Chapter10/WorkingWithEFCore/Northwind.cs
+++ Chapter10/WorkingWithEFCore/Northwind.cs
@@ -1,10 +1,14 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Sqlite;
+using WorkingWithEFCore;
 
 namespace Packt.Shared;
 //this manages the connection to the database
 public class Northwind : DbContext
 {
+    //these properties map to tables in the database
+    public DbSet<Category>? Categories { get; set; }
+    public DbSet<Product> Products { get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         string path = Path.Combine(Environment.CurrentDirectory, "Northwind.db");
@@ -14,5 +18,20 @@ public class Northwind : DbContext
         WriteLine($"Connection: {connection}");
         ForegroundColor = previousColor;
         optionsBuilder.UseSqlite(connection);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // example of using Fluent API instead of atributes
+        // to limit the length of a category name to 15
+        modelBuilder.Entity<Category>().Property(category => category.CategoryName)
+        .IsRequired() // not null
+        .HasMaxLength(15);
+        if (Database.ProviderName?.Contains("Sqlite") ?? false)
+        {
+            //added to "fix" te lack of decimal support in SQLite
+            modelBuilder.Entity<Product>().Property(product => product.Cost)
+            .HasConversion<double>();
+        }
     }
 }
