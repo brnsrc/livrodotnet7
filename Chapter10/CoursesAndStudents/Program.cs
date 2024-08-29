8e2536b519d57055af1def17365433059d7f01b6
Bruno de Souza <brunosrc@hotmail.com>
2024-08-29 00:46:26 -0300

    Code First EF Core models - Program

--- Chapter10/CoursesAndStudents/Program.cs
+++ Chapter10/CoursesAndStudents/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.EntityFrameworkCore; //for GenerateCreateScript()
+using Packt.Shared;
+
+using (Academy a = new())
+{
+    bool deleted = await a.Database.EnsureDeletedAsync();
+    WriteLine($"Databae deleted:{deleted}");
+
+    bool created = await a.Database.EnsureCreatedAsync();
+    WriteLine($"Database created: {created}");
+}
