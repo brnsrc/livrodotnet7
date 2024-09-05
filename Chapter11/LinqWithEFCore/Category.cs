2c7728e8f9a7a23b3416be1992b8856451abf84b
Bruno de Souza <brunosrc@hotmail.com>
2024-09-05 00:44:36 -0300

    Building an EF Core model

---
+++ Chapter11/LinqWithEFCore/Category.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LinqWithEFCore
+{
+    public class Category
+    {
+        
+    }
+}
