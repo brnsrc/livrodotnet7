1476d26360e09edca5460cb2dd438e4d609e2c7e
Bruno de Souza <brunosrc@hotmail.com>
2024-09-28 13:23:22 -0300

    Using shared layouts with Razor Pages

--- PracticalApps/Northwind.Web/Program.cs
+++ PracticalApps/Northwind.Web/Program.cs
@@ -1,4 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
+
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
@@ -6,7 +8,10 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 app.UseHttpsRedirection();
-app.MapGet("/", () => "Hello World!");
+app.UseDefaultFiles(); //index.xhtml, default.xhtml, and so on
+app.UseStaticFiles();
+app.MapRazorPages();
+app.MapGet("/hello", () => "Hello World!");
 
 app.Run();
 WriteLine("This executes after the web server has stopped!");
