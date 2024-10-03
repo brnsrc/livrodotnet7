7c041e26b1fa9b0ff7a7e94ee206f864963ece21
Bruno Souza <brunosrc@hotmail.com>
2024-10-03 19:10:20 -0300

    Reviewing the endpoint routing configuration in our project

--- PracticalApps/Northwind.Web/Program.cs
+++ PracticalApps/Northwind.Web/Program.cs
@@ -1,10 +1,12 @@
-using Packt.Shared; //NorthwindContext extensio method
+using Packt.Shared; //AddNorthwindContext extension method
 
+//configure services
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddNorthwindContext();
 var app = builder.Build();
 
+//configure the HTTP pipeline
 if (!app.Environment.IsDevelopment())
 {
     app.UseHsts();
@@ -14,6 +16,6 @@ app.UseDefaultFiles(); //index.xhtml, default.xhtml, and so on
 app.UseStaticFiles();
 app.MapRazorPages();
 app.MapGet("/hello", () => "Hello World!");
-
+//start the web server
 app.Run();
 WriteLine("This executes after the web server has stopped!");
