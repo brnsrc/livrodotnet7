85ebe31ac1cb004bb7e9fbc68e1e7f2bbf8bd2b0
Bruno Souza <brunosrc@hotmail.com>
2025-03-13 19:01:09 -0300

    Making other requests using REST Client pt2

--- PracticalApps/Northwind.Web/obj/Debug/net7.0/Northwind.Web.AssemblyInfo.cs
+++ PracticalApps/Northwind.Web/obj/Debug/net7.0/Northwind.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Northwind.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c8f68f75dfd8d85f56693e5775fe773eb57b9a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+528b472ada8316429c4eeda2d637b4cf25aacc02")]
 [assembly: System.Reflection.AssemblyProductAttribute("Northwind.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Northwind.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
